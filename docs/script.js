35443c08f8783eef869a1e408f81e5b9b9865c6e
ludothegeat <ludothegreat@gmail.com>
2023-09-14 08:24:35 -0500

    Add feature to combine key_counts.json from multiple branches

--- docs/script.js
+++ docs/script.js
@@ -1,45 +1,37 @@
-// script.js
+// JavaScript code to combine key_counts.json files from different GitHub branches
 
-// Fetch the key_counts.json file
-fetch('https://raw.githubusercontent.com/ludothegreat/KeyCount/ttztjohnsonm/key_counts.json')
-.then(response => response.json())
-.then(data => {
-    // Initialize an empty object to hold the summarized counts
-    const summarizedCounts = {
-        SingleKeys: {},
-        CombinationKeys: {}
-    };
+// URLs of the raw key_counts.json files from different branches
+const mainUrl = "https://raw.githubusercontent.com/ludothegreat/KeyCount/main/key_counts.json";
+const hotdogUrl = "https://raw.githubusercontent.com/ludothegreat/KeyCount/HotDog/key_counts.json";
+const ttztjohnsonmUrl = "https://raw.githubusercontent.com/ludothegreat/KeyCount/ttztjohnsonm/key_counts.json";
 
-    // Loop through the main categories (SingleKeys and CombinationKeys)
-    for (const category in data) {
-        if (data.hasOwnProperty(category)) {
-            // Loop through the keys within each category and summarize the counts
-            for (const key in data[category]) {
-                if (data[category].hasOwnProperty(key)) {
-                    const count = data[category][key];
-                    summarizedCounts[category][key] = (summarizedCounts[category][key] || 0) + count;
-                }
-            }
+// Function to combine key counts from multiple sources
+function combineKeyCounts(mainData, additionalData) {
+    for (const category in additionalData) {
+        if (!mainData.hasOwnProperty(category)) {
+            mainData[category] = {};
+        }
+        for (const key in additionalData[category]) {
+            mainData[category][key] = (mainData[category][key] || 0) + additionalData[category][key];
         }
     }
+    return mainData;
+}
 
-    // Display the summarized counts in the summary-container
+// Fetch and parse the key_counts.json data from each branch
+Promise.all([
+    fetch(mainUrl).then(response => response.json()),
+    fetch(hotdogUrl).then(response => response.json()),
+    fetch(ttztjohnsonmUrl).then(response => response.json())
+])
+.then(([mainData, hotdogData, ttztjohnsonmData]) => {
+    // Combine the key counts
+    const combinedData = combineKeyCounts(mainData, hotdogData);
+    combineKeyCounts(combinedData, ttztjohnsonmData);
+
+    // Display the combined data (for demonstration purposes, you can also save this to a file)
     const summaryContainer = document.getElementById('summary-container');
-    for (const category in summarizedCounts) {
-        if (summarizedCounts.hasOwnProperty(category)) {
-            const categoryDiv = document.createElement('div');
-            categoryDiv.textContent = `${category}:`;
-            summaryContainer.appendChild(categoryDiv);
-
-            for (const key in summarizedCounts[category]) {
-                if (summarizedCounts[category].hasOwnProperty(key)) {
-                    const element = document.createElement('div');
-                    element.textContent = `  - ${key}: ${summarizedCounts[category][key]}`;
-                    summaryContainer.appendChild(element);
-                }
-            }
-        }
-    }
+    summaryContainer.innerHTML = `<pre>${JSON.stringify(combinedData, null, 4)}</pre>`;
 })
 .catch(error => {
     console.error('An error occurred:', error);
