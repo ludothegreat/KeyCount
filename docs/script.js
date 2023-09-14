2774a1b23c41e87ee943627e3fd332faee2c7ef2
ludothegeat <ludothegreat@gmail.com>
2023-09-14 08:28:27 -0500

    removed main branch, it wasn't needed.

--- docs/script.js
+++ docs/script.js
@@ -1,33 +1,29 @@
-// JavaScript code to combine key_counts.json files from different GitHub branches
+// JavaScript code to combine key_counts.json files from the HotDog and ttztjohnsonm GitHub branches
 
 // URLs of the raw key_counts.json files from different branches
-const mainUrl = "https://raw.githubusercontent.com/ludothegreat/KeyCount/main/key_counts.json";
 const hotdogUrl = "https://raw.githubusercontent.com/ludothegreat/KeyCount/HotDog/key_counts.json";
 const ttztjohnsonmUrl = "https://raw.githubusercontent.com/ludothegreat/KeyCount/ttztjohnsonm/key_counts.json";
 
 // Function to combine key counts from multiple sources
-function combineKeyCounts(mainData, additionalData) {
-    for (const category in additionalData) {
-        if (!mainData.hasOwnProperty(category)) {
-            mainData[category] = {};
-        }
-        for (const key in additionalData[category]) {
-            mainData[category][key] = (mainData[category][key] || 0) + additionalData[category][key];
+function combineKeyCounts(data1, data2) {
+    const combinedData = {};
+    for (const category in data1) {
+        combinedData[category] = { ...data1[category] };
+        for (const key in data2[category]) {
+            combinedData[category][key] = (combinedData[category][key] || 0) + data2[category][key];
         }
     }
-    return mainData;
+    return combinedData;
 }
 
 // Fetch and parse the key_counts.json data from each branch
 Promise.all([
-    fetch(mainUrl).then(response => response.json()),
     fetch(hotdogUrl).then(response => response.json()),
     fetch(ttztjohnsonmUrl).then(response => response.json())
 ])
-.then(([mainData, hotdogData, ttztjohnsonmData]) => {
+.then(([hotdogData, ttztjohnsonmData]) => {
     // Combine the key counts
-    const combinedData = combineKeyCounts(mainData, hotdogData);
-    combineKeyCounts(combinedData, ttztjohnsonmData);
+    const combinedData = combineKeyCounts(hotdogData, ttztjohnsonmData);
 
     // Display the combined data (for demonstration purposes, you can also save this to a file)
     const summaryContainer = document.getElementById('summary-container');
